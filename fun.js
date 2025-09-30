6b6365c4afe509926a74ccab18285b13a8ab5b0a
Developer <developer@example.com>
2025-09-30 21:09:52 +0530

    Initial commit

---
+++ fun.js
@@ -0,0 +1,15 @@
+function greetUser(name) {
+    console.log("Hello, " + name + "!");
+  }
+  
+  greetUser("Lalitha"); 
+
+
+
+function hello(a, b) {
+    return (a + b);
+}
+
+let num1 = prompt("Enter first number:");
+let num2 = prompt("Enter second number:");
+console.log(hello(parseInt(num1), parseInt(num2)));
